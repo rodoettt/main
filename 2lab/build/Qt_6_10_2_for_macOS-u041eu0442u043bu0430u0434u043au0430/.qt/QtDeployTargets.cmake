39dc76b57520c35583d302eb5b178cbf25009f13
rodoettt <kotomanovtima@gmail.com>
2026-03-31 12:35:02 +0700

    вторая лабораторная

---
+++ 2lab/build/Qt_6_10_2_for_macOS-u041eu0442u043bu0430u0434u043au0430/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_2lab_FILE /Users/rodoet/Documents/study/2 семстрр/programming/2lab/build/Qt_6_10_2_for_macOS-u041eu0442u043bu0430u0434u043au0430/2lab.app/Contents/MacOS/2lab)
+set(__QT_DEPLOY_TARGET_2lab_TYPE EXECUTABLE)
